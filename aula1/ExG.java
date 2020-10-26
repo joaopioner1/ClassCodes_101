4d99f5965827abd26dbc36f4d4d49cd0943e05fa
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-26 00:13:56 -0300

    Update ExG.java

--- aula1/ExG.java
+++ aula1/ExG.java
@@ -3,13 +3,13 @@ package aula1;
 public class ExG {
 
 	public static void main(String[] args) {
-		System.out.println("     *");
-		System.out.println("    ***");		
-		System.out.println("   *****");		
-		System.out.println("  *******");		
-		System.out.println("    * *   ");		
-		System.out.println("    * * ");		
-		System.out.println(" *********");		
+		System.out.println("      *            ");
+		System.out.println("     ***           ");		
+		System.out.println("    *****          ");		
+		System.out.println("   *******         ");		
+		System.out.println("     * *           ");		
+		System.out.println("     * *           ");		
+		System.out.println("  *********        ");		
 	}
 
 }
