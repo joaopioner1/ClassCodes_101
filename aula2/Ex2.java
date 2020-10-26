c0ed31d0811488ad630a581db30d62e496dc8885
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-26 07:16:55 -0300

    Add files via upload

---
+++ aula2/Ex2.java
@@ -0,0 +1,23 @@
+package aula2;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Enter one number: ");
+		int number = in.nextInt();
+		
+		System.out.println("The number multiplied by 2 is: " + number * 2);
+		
+		in.close();
+	}
+
+}
+/*
+ * 2. Faça um programa capaz de obter um número inteiro do usuário. Então
+ * multiplique esse número por 2. Ao final, mostre o resultado na tela do
+ * computador.
+ */
