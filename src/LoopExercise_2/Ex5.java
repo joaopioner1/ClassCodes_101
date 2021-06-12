e9e37a3d1e987b1bd3e07357e3da395821ae7db1
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-06-12 20:18:24 -0300

    add ex 5

--- src/LoopExercise_2/Ex5.java
+++ src/LoopExercise_2/Ex5.java
@@ -1,5 +1,7 @@
 package LoopExercise_2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Ex5 {
@@ -7,25 +9,85 @@ public class Ex5 {
 	public static void main(String[] args) {
 		// Author: João Vítor Souza Pioner | Date: 11/06/2021
 		Scanner sc = new Scanner(System.in);
-		
-		Integer answer = 0;
+
+		List<Double> list = new ArrayList<Double>();
+
+		Double result = 0.0;
+		Integer answer = 0, cont = 0, quantity = 0;
 		do {
 			System.out.print("Você deseja comprar um produto (1-sim, 2-nao)? ");
-			answer = sc.nextInt();
-			System.out.print("Você deseja ver a lista de produtos? ");
+			cont = sc.nextInt();
+			if (cont == 1) {
+				System.out.print("Insira o código do produto: ");
+				answer = sc.nextInt();
+				switch (answer) {
+				case 123:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 0.20;
+					list.add(result);
+					break;
+				case 124:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 2.50;
+					list.add(result);
+					break;
+				case 125:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 0.25;
+					list.add(result);
+					break;
+				case 126:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 4.00;
+					list.add(result);
+					break;
+				case 127:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 4.50;
+					list.add(result);
+					break;
+				case 128:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 3.00;
+					list.add(result);
+					break;
+				case 129:
+					System.out.print("Insira a quantidade do produto: ");
+					quantity = sc.nextInt();
+					result = quantity * 2.50;
+					list.add(result);
+					break;
+				}
+			} else if (cont == 2) {
+				System.out.println("Ok, boa semana!");
+			} else {
+				throw new InvalidNumberEntered("Escreveu número errado. Digite 1/2");
+			}
+
+			System.out.print("Você deseja ver a lista de produtos (1-sim 2-nao)? ");
 			answer = sc.nextInt();
 			if (answer == 1) {
 				System.out.println(showList());
-			} 
-			System.out.print("Insira o código do produto: ");
-			
-			
-		} while (answer != 2);
-		
+			}
+
+		} while (cont != 2);
+
+		System.out.println("\nTamanho da lista: " + list.size());
+		Double i = 0.0;
+		for (Double aux : list) {
+			i += aux;
+		}
+		System.out.println("Valor total gasto: R$ " + String.format("%.2f", i));
 		
 		sc.close();
 	}
-	
+
 	public static String showList() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\nCódigo: 123 | Nome: Balas | Preço: R$ 0,20\n");
---
+++ src/LoopExercise_2/InvalidNumberEntered.java
@@ -0,0 +1,10 @@
+package LoopExercise_2;
+
+public class InvalidNumberEntered extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidNumberEntered(String msg) {
+		super(msg);
+	}
+
+}
