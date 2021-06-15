46e1d9009464422d4baf7c3f6a8294651bbe27c9
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-06-15 11:43:41 -0300

    Program working good now (showing quantities)

--- src/LoopExercise_2/Ex1.java
+++ src/LoopExercise_2/Ex1.java
@@ -4,7 +4,12 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 		//Author: João Vítor Souza Pioner | Date: 11/06/2021
-		try {
+		
+		Double[] d = new Double[5];
+		d[2] = 2.5;
+		System.out.println(d[1] + d[2]);
+		
+		/*try {
 			Integer i = 0;
 			while (i < 500) {
 				i++;
@@ -13,7 +18,7 @@ public class Ex1 {
 		}
 		catch (NullPointerException e) {
 			System.out.println("ERROR: " + e.getMessage());
-		}
+		}*/
 	}
 
 }
--- src/LoopExercise_2/Ex6.java
+++ src/LoopExercise_2/Ex6.java
@@ -10,69 +10,69 @@ public class Ex6 {
 		// Author: João Vítor Souza Pioner | Date: 14/06/2021
 		Scanner sc = new Scanner(System.in);
 		
-		List<Double> listValue = new ArrayList<>();
-		List<Integer> listQuantity = new ArrayList<>();
+		Integer q_xuxus=0, q_abacates=0, q_banana=0, q_morangos=0;
+		Integer answer = 0, i = 0;
 		
-		Integer answer = 0, quantityProduct = 0;
-		
+		Double[] prices = new Double[4];
 		Double valueProduct = 0.0;
 		System.out.println(showInfo());
 		do {
-			System.out.print("\nInsira um código: ");
+			System.out.print("\nInsira um código (0,1,2,3): ");
 			answer = sc.nextInt();
 			verfCode(answer);
-			switch (answer) {
-			case 0 :
+			if (answer == 0) {
 				System.out.println("GOODBYE!");
-				break;
-			case 1 : 
+			}
+			else if (answer == 1) {
 				System.out.print("\nInsira o código do produto: ");
 				Integer productCode = sc.nextInt();
 				switch (productCode) {
 				case 101 :
 					System.out.print("BANANA! Quantidade do produto: ");
-					quantityProduct = sc.nextInt();
-					valueProduct = quantityProduct * 0.20;
-					listValue.add(0, valueProduct);
-					listQuantity.add(0, quantityProduct);
-					continue;
+					q_banana = sc.nextInt();
+					valueProduct = q_banana * 0.20;
+					prices[i] = valueProduct;
+					q_banana += q_banana;
+					break;
 				case 201 :
 					System.out.print("XUXU! Quantidade do produto: ");
-					quantityProduct = sc.nextInt();
-					valueProduct = quantityProduct * 0.50;
-					listValue.add(1, valueProduct);
-					listQuantity.add(1, quantityProduct);
-					continue;
+					q_xuxus = sc.nextInt();
+					valueProduct = q_xuxus * 0.50;
+					prices[i] = valueProduct;
+					q_xuxus += q_xuxus;
+					break;
 				case 301 :
 					System.out.print("ABACATE! Quantidade do produto: ");
-					quantityProduct = sc.nextInt();
-					valueProduct = quantityProduct * 1.50;
-					listValue.add(2, valueProduct);
-					listQuantity.add(2, quantityProduct);
-					continue;
+					q_abacates = sc.nextInt();
+					valueProduct = q_abacates * 1.50;
+					prices[i] = valueProduct;
+					q_abacates += q_abacates;
+					break;
 				case 401 :
 					System.out.print("MORANGO! Quantidade do produto: ");
-					quantityProduct = sc.nextInt();
-					valueProduct = quantityProduct * 0.15;
-					listValue.add(3, valueProduct);
-					listQuantity.add(3, quantityProduct);
-					continue;
+					q_morangos = sc.nextInt();
+					valueProduct = q_morangos * 0.15;
+					prices[i] = valueProduct;
+					q_morangos += q_morangos;
+					break;
+					}
 				}
-			case 2 :
-				for (int i = 0; i <= listQuantity.size(); i++) {
-					System.out.println(showQuantity(listQuantity, i));
-				}
-				break;
-			case 3 :
-				System.out.printf("\nTotal R$ de bananas vendidas : %.2f", listValue.get(0));
-				System.out.println("Total R$ de xuxus vendidos: " + listValue.get(1));
-				System.out.println("Total R$ de abacates vendidos: " + listValue.get(2));
-				System.out.println("Total R$ de morangos vendidos   : " + listValue.get(3));
-				System.out.println("\nTotal R$ de produtos vendindos: " + valueProduct);
-				break;
+		
+			else if (answer == 2) {
+				System.out.println("\nQuantidade de bananas vendidas : " + q_banana);
+				System.out.println("Quantidade de xuxus vendidos: " + q_xuxus);
+				System.out.println("Quantidade de abacates vendidos: " + q_abacates);
+				System.out.println("Quantidade de morangos vendidos   : " + q_morangos);
 			}
-		} 
-		while (answer != 0);
+			else if (answer == 3) {
+				System.out.printf("\nTotal R$ de bananas vendidas : %.2f", prices[i]);
+				System.out.printf("\nTotal R$ de xuxus vendidos: %.2f", prices[i]);
+				System.out.printf("\nTotal R$ de abacates vendidos: %.2f", prices[i]);
+				System.out.printf("\nTotal R$ de morangos vendidos   : %.2f", prices[i]);
+				System.out.printf("\nTotal R$ de produtos vendindos: %.2f", valueProduct);
+			}
+			i++;
+		} while (answer != 0);
 		
 		sc.close();
 	}
@@ -89,7 +89,7 @@ public class Ex6 {
 		sb.append("\nBanana   - 101 | UNI R$ 0.20");
 		sb.append("\nXuxu     - 201 | UNI R$ 0.50");
 		sb.append("\nAbacate  - 301 | UNI R$ 1.50");
-		sb.append("\nTMorango - 401 | UNI R$ 0.15");
+		sb.append("\nMorango  - 401 | UNI R$ 0.15");
 		return sb.toString();
 	}
 	
@@ -97,21 +97,5 @@ public class Ex6 {
 		if (code != 0 && code != 1 && code != 2 && code != 3) {
 			throw new InvalidNumberEntered("Voce precisa digitar inicialmente os codigos 0, 1, 2 ou 3");
 		}
-	}
-	
-	static String showQuantity(List<Integer> list, int i) {
-		if (list.get(0) != null) {
-			return "\nQuantidade de bananas vendidas : " + list.get(i);
-		} 
-		if (list.get(1) != null) {
-			return "\nQuantidade de xuxus vendidos: " + list.get(i);
-		} 
-		if (list.get(2) != null) {
-			return "\nQuantidade de abacates vendidos: " + list.get(i);
-		} 
-		if (list.get(3) != null) {
-			return "\nQuantidade de morangos vendidos: " + list.get(i);
-		} 
-		return null;
 	}
 }
